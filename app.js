ac0a7a7a4adb7f211e857422e0761115de96b795
Jinwei Lin <jinwei.lin8@gmail.com>
2017-01-16 15:01:44 -0500

    set up basic route handling

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/players', (req,res) => {
+  request('https://api.sportradar.com/tennis-t2/en/players/sr:competitor:18378/profile.json?api_key=t59wap9bzndhdx5wnqm92uh7', (error, res, body) =>{
+    if(!error && res.statusCode === 200) {
+      console.log(body);
+    }
+  });
+  res.render('players');
+});
+
+app.listen(8000, () => {
+  console.log('Tennis Enthusiast server has started!');
+});
